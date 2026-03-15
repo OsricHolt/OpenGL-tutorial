1e797205c49757b86941adedad4e84805d759aac
Osric Holt <osricholt@gmail.com>
2026-03-15 15:58:43 -0700

    Easy Exercise 2: Change window size to 400x225

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
 	// Tell GLFW we are using Core profile (vs. compatibility profile that has outdated commands to be back-compatible)
 
 	// create a window object with GLFW window datatype with a size of 800x800 pixels named "OpenGL Tutorial"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL Tutorial", NULL, NULL); // datatype* (width, height, name, fullscreen?, not important)
+	GLFWwindow* window = glfwCreateWindow(400, 225, "OpenGL Tutorial", NULL, NULL); // datatype* (width, height, name, fullscreen?, not important)
 
 	//check to see if the window had an error generating (and if it does close it)
 	if (window == NULL){
@@ -27,7 +27,7 @@ int main() {
 	// Load glad into OpenGL
 	gladLoadGL();
 
-	glViewport(0,0, 800,800); // area of OpenGL we want to render in from an (x, y) of (0, 0) to (800, 800)
+	glViewport(0, 0, 400, 225); // area of OpenGL we want to render in from an (x, y) of (0, 0) to (800, 800)
 
 	glClearColor(1.0f, 0.4f, 0.0f, 1.0f); //Orange; prepare to clear color and replace it (color back buffer)
 	glClear(GL_COLOR_BUFFER_BIT); // configure buffer swap for color buffer
